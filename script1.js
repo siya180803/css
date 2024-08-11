d6b311e5c7eb5126bf01728f090473d8d053907a
Nikunj Agarwal <nikunjagarwal0711@gmail.com>
2024-08-11 20:14:18 +0530

    created

---
+++ script1.js
@@ -0,0 +1,24 @@
+const hrt = document.querySelector('.heart-Circle');
+
+const toggle = function () {
+	if ( hrt.getAttribute('aria-checked') === 'true' ) {
+		hrt.setAttribute('aria-checked', 'false');
+	}
+	else {
+		hrt.setAttribute('aria-checked', 'true');
+	}
+}
+
+hrt.addEventListener('click', toggle, false);
+hrt.addEventListener('keypress', function ( e ) {
+	const keyCode = e.keyCode || e.which;
+	switch ( keyCode ) {
+		case 32:
+			e.preventDefault();
+			toggle();
+			break;
+		default:
+			break;
+	}
+	
+})
